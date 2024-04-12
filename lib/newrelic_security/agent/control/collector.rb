2d834bff533a666172579ff8bbbae4cded005b2f
Prateek Sen <psen@newrelic.com>
2024-04-12 12:41:35 +0530

    update apiId format to '<case_type>-<apiId>'

--- lib/newrelic_security/agent/control/collector.rb
+++ lib/newrelic_security/agent/control/collector.rb
@@ -50,7 +50,7 @@ module NewRelic::Security
               event.stacktrace << route
             end
           end
-          event.apiId = calculate_api_id(event.stacktrace, event.httpRequest[:method])
+          event.apiId = "#{case_type}-#{calculate_api_id(event.stacktrace, event.httpRequest[:method])}"
           NewRelic::Security::Agent.agent.event_processor.send_event(event)
           if event.httpRequest[:headers].key?(NR_CSEC_FUZZ_REQUEST_ID) && event.apiId == event.httpRequest[:headers][NR_CSEC_FUZZ_REQUEST_ID].split(COLON_IAST_COLON)[0]
             NewRelic::Security::Agent.agent.iast_client.completed_requests[event.parentId] << event.id
