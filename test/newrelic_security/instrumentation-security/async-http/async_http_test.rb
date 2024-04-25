8eccf082e53aeb19c0761414654b81c37809b394
Prateek Sen <psen@newrelic.com>
2024-04-25 13:51:27 +0530

    fix for ensure_error in ruby 2.4 for async-http unit tests

--- test/newrelic_security/instrumentation-security/async-http/async_http_test.rb
+++ test/newrelic_security/instrumentation-security/async-http/async_http_test.rb
@@ -14,12 +14,17 @@ module NewRelic::Security
                     args = [{:Method=>"GET", :scheme=>"http", :host=>"www.google.com", :port=>80, :URI=>"http://www.google.com/search?q=test", :path=>"/search", :query=>"q=test", :Body=>"", :Headers=>{"accept"=>"application/json"}}]
                     response = nil
                     Async do
-                        internet = Async::HTTP::Internet.new
-                        headers = [['accept', 'application/json']]
-                        response = internet.get "http://www.google.com/search?q=test", headers
-                        response.read
-                    ensure
-                        internet.close
+                        begin
+                            internet = Async::HTTP::Internet.new
+                            headers = [['accept', 'application/json']]
+                            response = internet.get "http://www.google.com/search?q=test", headers
+                            response.read
+                        rescue Exception => e
+                            NewRelic::Security::Agent.logger.debug "Exception in TestAsyncHTTP.test_get : #{e} #{e.backtrace}"
+                        ensure
+                            internet.close
+                        end
+                        # TODO: Ensure without begin/end supported after RUBY_VERSION >= 2.5, remove rescue to optimize.
                     end
                     assert_equal 200, response.status
                     expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
@@ -33,29 +38,35 @@ module NewRelic::Security
                     assert_nil expected_event.eventCategory, $event_list[0].eventCategory
                 end
 
-                def test_get_ssl
-                    $event_list.clear()
-                    url = "http://www.google.com"
-                    args = [{:Method=>"GET", :scheme=>"https", :host=>"www.google.com", :port=>443, :URI=>"https://www.google.com/search?q=test", :path=>"/search", :query=>"q=test", :Body=>"", :Headers=>{"accept"=>"application/json"}}]
-                    response = nil
-                    Async do
-                        internet = Async::HTTP::Internet.new
-                        headers = [['accept', 'application/json']]
-                        response = internet.get "https://www.google.com/search?q=test", headers
-                        response.read
-                    ensure
-                        internet.close
+                if RUBY_VERSION >= '2.5.0'
+                    def test_get_ssl
+                        $event_list.clear()
+                        url = "http://www.google.com"
+                        args = [{:Method=>"GET", :scheme=>"https", :host=>"www.google.com", :port=>443, :URI=>"https://www.google.com/search?q=test", :path=>"/search", :query=>"q=test", :Body=>"", :Headers=>{"accept"=>"application/json"}}]
+                        response = nil
+                        Async do
+                            begin
+                                internet = Async::HTTP::Internet.new
+                                headers = [['accept', 'application/json']]
+                                response = internet.get "https://www.google.com/search?q=test"#, headers
+                                response.read
+                            rescue Exception => e
+                                NewRelic::Security::Agent.logger.debug "Exception in TestAsyncHTTP.test_get_ssl : #{e} #{e.backtrace}"
+                            ensure
+                                internet.close
+                            end
+                        end
+                        assert_equal 200, response.status
+                        expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
+                        assert_equal 1, NewRelic::Security::Agent::Control::Collector.get_event_count(HTTP_REQUEST)
+                        assert_equal expected_event.caseType, $event_list[0].caseType
+                        assert_equal expected_event.parameters[0][:URI], $event_list[0].parameters[0][:URI]
+                        assert_equal expected_event.parameters[0][:Method], $event_list[0].parameters[0][:Method]
+                        assert_equal expected_event.parameters[0][:scheme], $event_list[0].parameters[0][:scheme]
+                        assert_equal expected_event.parameters[0][:port], $event_list[0].parameters[0][:port]
+                        assert_equal expected_event.parameters[0][:Body], $event_list[0].parameters[0][:Body]
+                        assert_nil expected_event.eventCategory, $event_list[0].eventCategory
                     end
-                    assert_equal 200, response.status
-                    expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
-                    assert_equal 1, NewRelic::Security::Agent::Control::Collector.get_event_count(HTTP_REQUEST)
-                    assert_equal expected_event.caseType, $event_list[0].caseType
-                    assert_equal expected_event.parameters[0][:URI], $event_list[0].parameters[0][:URI]
-                    assert_equal expected_event.parameters[0][:Method], $event_list[0].parameters[0][:Method]
-                    assert_equal expected_event.parameters[0][:scheme], $event_list[0].parameters[0][:scheme]
-                    assert_equal expected_event.parameters[0][:port], $event_list[0].parameters[0][:port]
-                    assert_equal expected_event.parameters[0][:Body], $event_list[0].parameters[0][:Body]
-                    assert_nil expected_event.eventCategory, $event_list[0].eventCategory
                 end
 
                 def test_post_json
@@ -65,13 +76,17 @@ module NewRelic::Security
                     data = {"title"=>"New", "author"=>"New Author"}
                     response = nil
                     Async do
-                        internet = Async::HTTP::Internet.new
-                        headers = [['Content-Type', 'application/json']]
-                        body = [JSON.dump(data)]
-                        response = internet.post url, headers, body
-                        response.read
-                    ensure
-                        internet.close
+                        begin
+                            internet = Async::HTTP::Internet.new
+                            headers = [['Content-Type', 'application/json']]
+                            body = [JSON.dump(data)]
+                            response = internet.post url, headers, body
+                            response.read
+                        rescue Exception => e
+                            NewRelic::Security::Agent.logger.debug "Exception in TestAsyncHTTP.test_post_json : #{e} #{e.backtrace}"
+                        ensure
+                            internet.close
+                        end
                     end
                     assert_equal 201, response.status
                     expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
@@ -92,13 +107,17 @@ module NewRelic::Security
                     data = {"title"=>"Update Book","author"=>"Update Author"}
                     response = nil
                     Async do
-                        internet = Async::HTTP::Internet.new
-                        headers = [['Content-Type', 'application/json']]
-                        body = [JSON.dump(data)]
-                        response = internet.put url, headers, body
-                        response.read
-                    ensure
-                        internet.close
+                        begin
+                            internet = Async::HTTP::Internet.new
+                            headers = [['Content-Type', 'application/json']]
+                            body = [JSON.dump(data)]
+                            response = internet.put url, headers, body
+                            response.read
+                        rescue Exception => e
+                            NewRelic::Security::Agent.logger.debug "Exception in TestAsyncHTTP.test_put_json : #{e} #{e.backtrace}"
+                        ensure
+                            internet.close
+                        end
                     end
                     assert_equal 200, response.status
                     expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
@@ -118,11 +137,15 @@ module NewRelic::Security
                     args = [{:Method=>"DELETE", :scheme=>"http", :host=>"localhost", :port=>9291, :URI=>"http://localhost:9291/books/1", :path=>"/books/1", :query=>nil, :Body=>"", :Headers=>{}}]
                     response = nil
                     Async do
-                        internet = Async::HTTP::Internet.new
-                        response = internet.delete url
-                        response.read
-                    ensure
-                        internet.close
+                        begin
+                            internet = Async::HTTP::Internet.new
+                            response = internet.delete url
+                            response.read
+                        rescue Exception => e
+                            NewRelic::Security::Agent.logger.debug "Exception in TestAsyncHTTP.test_delete_json : #{e} #{e.backtrace}"
+                        ensure
+                            internet.close
+                        end
                     end
                     assert_equal 204, response.status
                     expected_event = NewRelic::Security::Agent::Control::Event.new(HTTP_REQUEST, args, nil)
