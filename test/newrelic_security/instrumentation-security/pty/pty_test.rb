02713853549af5ad3a6bcb709ab6a26877f4dbab
Manoj Yadav <myadav@newrelic.com>
2024-07-04 16:46:47 +0530

    Added output data verification in test_getpty case

--- test/newrelic_security/instrumentation-security/pty/pty_test.rb
+++ test/newrelic_security/instrumentation-security/pty/pty_test.rb
@@ -6,6 +6,8 @@ module NewRelic::Security
   module Test
     module Instrumentation
       class TestPTY < Minitest::Test
+        TEST_DIR_PATH = __dir__
+
         def setup
           $event_list.clear()
         end
@@ -21,14 +23,20 @@ module NewRelic::Security
         end
 
         def test_getpty
-          cmd = "date"
+          cmd = "ls " + TEST_DIR_PATH
+          output = ""
           PTY::getpty("#{cmd}") do |reader, writer, pid|
             begin
-              reader.readline
+              output = reader.readline
             rescue
               break
             end
           end
+          # output data verification
+          output_filename = output.strip
+          expected_filename = "pty_test.rb"
+          assert_equal expected_filename, output_filename
+          # event verification
           expected_event = NewRelic::Security::Agent::Control::Event.new(SYSTEM_COMMAND, [cmd], nil)
           assert_equal 1, NewRelic::Security::Agent::Control::Collector.get_event_count(SYSTEM_COMMAND)
           assert_equal expected_event.caseType, $event_list[0].caseType
