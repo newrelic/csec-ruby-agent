3f02c1f9b895bffc8b261488ab40d4995648dbe2
Prateek Sen <psen@newrelic.com>
2024-07-15 14:23:05 +0530

    PostgreSQLAdapter.execute hook not required as PG::async_exec hook is added

--- lib/newrelic_security/instrumentation-security/active_record/postgresql_adapter/chain.rb
+++ lib/newrelic_security/instrumentation-security/active_record/postgresql_adapter/chain.rb
@@ -9,14 +9,6 @@ module NewRelic::Security
               ::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.class_eval do
                 include NewRelic::Security::Instrumentation::ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
 
-                alias_method :execute_without_security, :execute
-                
-                def execute(sql, name = nil)
-                  retval = nil
-                  event = execute_on_enter(sql, name) { retval = execute_without_security(sql, name) }
-                  execute_on_exit(event) { return retval }
-                end
-
                 alias_method :exec_query_without_security, :exec_query
 
                 if ::Rails.version < '5'
