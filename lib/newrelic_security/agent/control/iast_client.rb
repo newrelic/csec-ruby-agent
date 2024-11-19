b46073f23396e5d4ef1f49abc6593be284a7ed46
Prateek Sen <psen@newrelic.com>
2024-11-19 12:08:01 +0530

    nil check for event processor

--- lib/newrelic_security/agent/control/iast_client.rb
+++ lib/newrelic_security/agent/control/iast_client.rb
@@ -86,7 +86,7 @@ module NewRelic::Security
                 iast_data_transfer_request.batchSize = batch_size * 2
                 iast_data_transfer_request.pendingRequestIds = pending_request_ids.to_a
                 iast_data_transfer_request.completedRequests = completed_requests
-                NewRelic::Security::Agent.agent.event_processor.send_iast_data_transfer_request(iast_data_transfer_request)
+                NewRelic::Security::Agent.agent.event_processor&.send_iast_data_transfer_request(iast_data_transfer_request)
               end
             end
           end
