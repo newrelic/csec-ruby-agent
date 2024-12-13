e54e7f55c007e66822551c9e4962ca53ff64c85c
Prateek Sen <33506953+prateeksen@users.noreply.github.com>
2024-12-13 13:14:21 +0530

    fix do not send empty url endpoint json (#151)

--- lib/newrelic_security/agent/control/control_command.rb
+++ lib/newrelic_security/agent/control/control_command.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'securerandom'
 
 module NewRelic::Security
   module Agent
@@ -93,7 +94,7 @@ module NewRelic::Security
           NewRelic::Security::Agent.agent.iast_client.completed_requests.clear if NewRelic::Security::Agent.agent.iast_client
           NewRelic::Security::Agent.agent.iast_client.pending_request_ids.clear if NewRelic::Security::Agent.agent.iast_client
           NewRelic::Security::Agent.config.disable_security
-          Thread.new { NewRelic::Security::Agent.agent.reconnect(0) }
+          Thread.new { NewRelic::Security::Agent.agent.reconnect(SecureRandom.random_number(10) + 5) }
         end
 
         def current_time_millis
