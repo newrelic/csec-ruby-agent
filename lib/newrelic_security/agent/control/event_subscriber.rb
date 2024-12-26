9fa3f293964f36ee16a05f5bacb8691dc357131f
Prateek Sen <psen@newrelic.com>
2024-12-26 13:37:36 +0530

    clearing to_json residual and csec agent main thread

--- lib/newrelic_security/agent/control/event_subscriber.rb
+++ lib/newrelic_security/agent/control/event_subscriber.rb
@@ -8,7 +8,11 @@ module NewRelic::Security
               NewRelic::Security::Agent.init_logger.info "NewRelic server_source_configuration_added for pid : #{Process.pid}, Parent Pid : #{Process.ppid}"
               NewRelic::Security::Agent.config.update_server_config
               if NewRelic::Security::Agent.config[:'security.enabled'] && !NewRelic::Security::Agent.config[:high_security]
-                Thread.new { NewRelic::Security::Agent.agent.scan_scheduler.init_via_scan_scheduler }
+                NewRelic::Security::Agent.agent.event_processor&.event_dequeue_threads&.each { |t| t&.kill }
+                NewRelic::Security::Agent.agent.event_processor = nil
+                @csec_agent_main_thread&.kill
+                @csec_agent_main_thread = nil
+                @csec_agent_main_thread = Thread.new { NewRelic::Security::Agent.agent.scan_scheduler.init_via_scan_scheduler }
               else
                 NewRelic::Security::Agent.logger.info "New Relic Security is disabled by one of the user provided config `security.enabled` or `high_security`."
                 NewRelic::Security::Agent.init_logger.info "New Relic Security is disabled by one of the user provided config `security.enabled` or `high_security`."
